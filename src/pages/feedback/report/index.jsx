841d28f07cccc6a18664af65ed8a3c1c7e2b3ba6
alejorendons <diegoo.alejo74@gmail.com>
2025-05-18 14:51:52 -0500

    Update Pages

---
+++ src/pages/feedback/report/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Index = () => {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Hola mundo</h1>
+      <p>Este es un componente básico.</p>
+    </div>
+  );
+};
+
+export default Index;
